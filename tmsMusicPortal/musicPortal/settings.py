0886528b4ddb1d08a979ebb39250bad735dc2d60
tesslvolt <alex7887500@gmail.com>
2025-11-12 14:03:50 +0300

    Меняем СУБД на SQLite

--- tmsMusicPortal/musicPortal/settings.py
+++ tmsMusicPortal/musicPortal/settings.py
@@ -103,12 +103,8 @@ load_dotenv(BASE_DIR / '.env')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # база будет создаваться в корне проекта
     }
 }
 
